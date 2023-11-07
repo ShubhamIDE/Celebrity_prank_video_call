7f2a3dac0c68a7b5d621f0b53fbbca5db6e41a19
shubham thorat <shubham230520011@gmail.com>
2023-11-07 11:56:59 +0530

    added some code

---
+++ app/src/main/java/com/videocall/livecelebrity/prankcall/splash/LanguageActivity.kt
@@ -0,0 +1,12 @@
+package com.videocall.livecelebrity.prankcall.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.videocall.livecelebrity.prankcall.R
+
+class LanguageActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_language)
+    }
+}
